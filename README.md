5cdc589c12b44912bdf5d9b06647700551a10a4e
guillaumeRequena <guillaume.r@ecotree.fr>
2025-02-01 17:30:22 +0100

    add composer require in readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Voir les classes GildedRose.php et GildedRoseTest.php
+Voir les classes GildedRose.php et GildedRoseTest.php  
+https://github.com/approvals/ApprovalTests.php  
+composer req --dev approvals/approval-tests
--- src/GildedRose.php
+++ src/GildedRose.php
@@ -14,54 +14,158 @@ final class GildedRose
     ) {
     }
 
+    // public function updateQuality(): void
+    // {
+    //     foreach ($this->items as $item) {
+    //         if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+    //             if ($item->quality > 0) {
+    //                 if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+    //                     $item->quality = $item->quality - 1;
+    //                 }
+    //             }
+    //         } else {
+    //             if ($item->quality < 50) {
+    //                 $item->quality = $item->quality + 1;
+    //                 if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+    //                     if ($item->sellIn < 11) {
+    //                         if ($item->quality < 50) {
+    //                             $item->quality = $item->quality + 1;
+    //                         }
+    //                     }
+    //                     if ($item->sellIn < 6) {
+    //                         if ($item->quality < 50) {
+    //                             $item->quality = $item->quality + 1;
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+
+    //         if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+    //             $item->sellIn = $item->sellIn - 1;
+    //         }
+
+    //         if ($item->sellIn < 0) {
+    //             if ($item->name != 'Aged Brie') {
+    //                 if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+    //                     if ($item->quality > 0) {
+    //                         if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+    //                             $item->quality = $item->quality - 1;
+    //                         }
+    //                     }
+    //                 } else {
+    //                     $item->quality = $item->quality - $item->quality;
+    //                 }
+    //             } else {
+    //                 if ($item->quality < 50) {
+    //                     $item->quality = $item->quality + 1;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    const AGED_BRIE = 'Aged Brie';
+    const BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
+    const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+
+
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            /** @var Item $item */
+            if (!in_array($item->name, [self::AGED_BRIE, self::BACKSTAGE])) {
                 if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+                    if ($item->name != self::SULFURAS) {
+                        $item->quality--;
                     }
                 }
             } else {
                 if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    $item->quality++;
+                    if ($item->name == self::BACKSTAGE) {
                         if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                            $this->incrementQualityIfInferiorToFifty($item);
                         }
                         if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                            $this->incrementQualityIfInferiorToFifty($item);
                         }
                     }
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
+            if ($item->name != self::SULFURAS) {
+                $item->sellIn--;
             }
 
+            // finir ici
             if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->name != self::AGED_BRIE) {
+                    if ($item->name != self::BACKSTAGE) {
                         if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
+                            if ($item->name != self::SULFURAS) {
+                                $item->quality--;
                             }
                         }
                     } else {
-                        $item->quality = $item->quality - $item->quality;
+                        $item->quality = 0;
                     }
                 } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+                    $this->incrementQualityIfInferiorToFifty($item);
                 }
             }
         }
     }
+
+    private function updateQualityAgedBrie(Item $item): void
+    {
+        $this->incrementQualityIfInferiorToFifty($item);
+        $item->sellIn--;
+
+        if($item->sellIn < 0){
+            $this->incrementQualityIfInferiorToFifty($item);
+        }
+    }
+
+    private function updateQualityBackstage(Item $item): void
+    {
+        $this->incrementQualityIfInferiorToFifty($item);
+
+        if ($item->sellIn < 11) {
+            $this->incrementQualityIfInferiorToFifty($item);
+        }
+
+        if ($item->sellIn < 6) {
+            $this->incrementQualityIfInferiorToFifty($item);
+        }
+
+        $item->sellIn--;
+        $item->quality = 0;
+    }
+
+    private function updateQualitySulfuras(Item $item): void
+    {
+        
+    }
+
+    private function updateQualityOther(Item $item): void
+    {
+        if ($item->quality > 0) {
+            $item->quality--;
+        }
+
+        $item->sellIn--;
+
+        if ($item->sellIn < 0 && $item->quality > 0) {
+            $item->quality--;
+        }
+    }
+
+    private function incrementQualityIfInferiorToFifty(Item $item): void
+    {
+        if ($item->quality < 50) {
+            $item->quality++;
+        }
+    }
+
 }
